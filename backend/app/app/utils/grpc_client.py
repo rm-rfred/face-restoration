ac9330057e0fddc3eae06b4e6f1b6426cad0528a
Frédéric Thierrée <fredericthierree@gmail.com>
2023-12-26 14:48:58 +0100

    Updating grpc client with all arguments

--- backend/app/app/utils/grpc_client.py
+++ backend/app/app/utils/grpc_client.py
@@ -1,20 +1,41 @@
 import grpc
 
-from app.config import face_restoration_pb2, face_restoration_pb2_grpc
+from app.grpc_config import face_restoration_pb2, face_restoration_pb2_grpc
+
 
 class GrpcClient:
     @staticmethod
-    def get_face_restoration_from_grpc(endpoint: str, image: bytes, timeout: int = 60):
+    def get_face_restoration_from_grpc(
+        endpoint: str,
+        image: bytes,
+        background_enhance: bool = False,
+        face_upsample: bool = True,
+        upscale: int = 2,
+        codeformer_fidelity: float = 0.7,
+        timeout: int = 60,
+    ):
         return GrpcClient.face_restoration(
-            endpoint=endpoint, image=image, timeout=timeout
+            endpoint=endpoint,
+            image=image,
+            background_enhance=background_enhance,
+            face_upsample=face_upsample,
+            upscale=upscale,
+            codeformer_fidelity=codeformer_fidelity,
+            timeout=timeout,
         )
-    
+
     @staticmethod
     def face_restoration(
-        endpoint: str, image, timeout: int=60
+        endpoint: str,
+        image,
+        background_enhance: bool = False,
+        face_upsample: bool = True,
+        upscale: int = 2,
+        codeformer_fidelity: float = 0.7,
+        timeout: int = 60,
     ):
         """Apply face restoration on image face
-        
+
         Arguments:
             endpoint (str): Server endpoint
             image: The image to apply face restoration
@@ -23,17 +44,22 @@ class GrpcClient:
         channel = grpc.insecure_channel(
             endpoint,
             options=[
-                ('grpc.max_send_message_length', -1),
-                ('grpc.max_receive_message_length', -1),
-                ('grpc.so_reuseport', 1),
-                ('grpc.use_local_subchannel_pool', 1),
-            ]
+                ("grpc.max_send_message_length", -1),
+                ("grpc.max_receive_message_length", -1),
+                ("grpc.so_reuseport", 1),
+                ("grpc.use_local_subchannel_pool", 1),
+            ],
         )
         stub = face_restoration_pb2_grpc.FaceRestorationServiceStub(channel)
 
         response = stub.ApplyFaceRestoration(
             face_restoration_pb2.FaceRestorationRequest(
                 image=image,
-            ), timeout=timeout
+                background_enhance=background_enhance,
+                face_upsample=face_upsample,
+                upscale=upscale,
+                codeformer_fidelity=codeformer_fidelity,
+            ),
+            timeout=timeout,
         )
         return response.restored_image
